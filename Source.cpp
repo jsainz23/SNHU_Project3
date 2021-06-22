58ee95c27aa2bda7328037fa1fb9dfb10342b6a4
jsainz23 <85897937+jsainz23@users.noreply.github.com>
2021-06-21 22:28:15 -0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,203 @@
+#include <Python.h>
+#include <iostream>
+#include <fstream>
+#include <Windows.h>
+#include <cmath>
+#include <string>
+#include <Vector>
+
+using namespace std;
+
+/*
+Description:
+	To call this function, simply pass the function name in Python that you wish to call.
+Example:
+	callProcedure("printsomething");
+Output:
+	Python will print on the screen: Hello from python!
+Return:
+	None
+*/
+void CallProcedure(string pName)
+{
+	char* procname = new char[pName.length() + 1];
+	std::strcpy(procname, pName.c_str());
+
+	Py_Initialize();
+	PyObject* my_module = PyImport_ImportModule("myfirstprogram");
+	PyErr_Print();
+	PyObject* my_function = PyObject_GetAttrString(my_module, procname);
+	PyObject* my_result = PyObject_CallObject(my_function, NULL);
+	Py_Finalize();
+
+	delete[] procname;
+}
+
+/*
+Description:
+	To call this function, pass the name of the Python functino you wish to call and the string parameter you want to send
+Example:
+	int x = callIntFunc("PrintMe","Test");
+Output:
+	Python will print on the screen:
+		You sent me: Test
+Return:
+	100 is returned to the C++
+*/
+int callIntFunc(string proc, string param)
+{
+	char* procname = new char[proc.length() + 1];
+	std::strcpy(procname, proc.c_str());
+
+	char* paramval = new char[param.length() + 1];
+	std::strcpy(paramval, param.c_str());
+
+
+	PyObject* pName, * pModule, * pDict, * pFunc, * pValue = nullptr, * presult = nullptr;
+	// Initialize the Python Interpreter
+	Py_Initialize();
+	// Build the name object
+	pName = PyUnicode_FromString((char*)"myfirstprogram");
+	// Load the module object
+	pModule = PyImport_Import(pName);
+	// pDict is a borrowed reference 
+	pDict = PyModule_GetDict(pModule);
+	// pFunc is also a borrowed reference 
+	pFunc = PyDict_GetItemString(pDict, procname);
+	if (PyCallable_Check(pFunc))
+	{
+		pValue = Py_BuildValue("(z)", paramval);
+		PyErr_Print();
+		presult = PyObject_CallObject(pFunc, pValue);
+		PyErr_Print();
+	}
+	else
+	{
+		PyErr_Print();
+	}
+	//printf("Result is %d\n", _PyLong_AsInt(presult));
+	Py_DECREF(pValue);
+	// Clean up
+	Py_DECREF(pModule);
+	Py_DECREF(pName);
+	// Finish the Python Interpreter
+	Py_Finalize();
+
+	// clean 
+	delete[] procname;
+	delete[] paramval;
+
+
+	return _PyLong_AsInt(presult);
+}
+
+/*
+Description:
+	To call this function, pass the name of the Python functino you wish to call and the string parameter you want to send
+Example:
+	int x = callIntFunc("doublevalue",5);
+Return:
+	25 is returned to the C++
+*/
+int callIntFunc(string proc, int param)
+{
+	char* procname = new char[proc.length() + 1];
+	std::strcpy(procname, proc.c_str());
+
+	PyObject* pName, * pModule, * pDict, * pFunc, * pValue = nullptr, * presult = nullptr;
+	// Initialize the Python Interpreter
+	Py_Initialize();
+	// Build the name object
+	pName = PyUnicode_FromString((char*)"myfirstprogram");
+	// Load the module object
+	pModule = PyImport_Import(pName);
+	// pDict is a borrowed reference 
+	pDict = PyModule_GetDict(pModule);
+	// pFunc is also a borrowed reference 
+	pFunc = PyDict_GetItemString(pDict, procname);
+	if (PyCallable_Check(pFunc))
+	{
+		pValue = Py_BuildValue("(i)", param);
+		PyErr_Print();
+		presult = PyObject_CallObject(pFunc, pValue);
+		PyErr_Print();
+	}
+	else
+	{
+		PyErr_Print();
+	}
+	//printf("Result is %d\n", _PyLong_AsInt(presult));
+	Py_DECREF(pValue);
+	// Clean up
+	Py_DECREF(pModule);
+	Py_DECREF(pName);
+	// Finish the Python Interpreter
+	Py_Finalize();
+
+	// clean 
+	delete[] procname;
+
+	return _PyLong_AsInt(presult);
+}
+
+//function to display the selection menu
+void displayMenu() {
+
+	cout << "1: Print Item List " << endl << "2: Search for frequency of specific item " << endl << "3: Print all items and frequency of items as a histogram" << endl << "4: Quit" << endl << endl << "Enter your selection as a number 1,2,3 or 4: ";
+}
+
+//function to turn the item vector into a string to be parsed to python later
+string createString(vector<string> foodVector) {
+
+	string foodString = "";
+	int i;
+	for (i = 0; i < foodVector.size(); i++) {
+
+		foodString = foodString + " " + foodVector[i];
+
+
+	}
+
+	return foodString;
+}
+
+void main()
+{
+	//decleration of food vector
+	vector<string> foodVector;
+	int answer = 0;
+	//fill food vector
+	fstream newfile;
+	newfile.open("input.txt", ios::in); //open a file to perform read operation using file object
+	if (newfile.is_open()) {   //checking whether the file is open
+		string tp;
+		while (getline(newfile, tp)) { //read data from file object and put it into string.
+			foodVector.push_back(tp);
+		}
+		newfile.close(); //close the file object.
+	}
+	//running program loop
+	while (answer != 4) {
+
+		displayMenu();
+		cin >> answer;
+		if (answer == 1) {
+			//calls pthon function individualCount
+			int x = callIntFunc("individualCount", createString(foodVector));
+
+		}
+		if (answer == 2) {
+			//calls python function searchCount
+			int x = callIntFunc("searchCount", createString(foodVector));
+		}
+		if (answer == 3) {
+			//calls python function histo
+			int x = callIntFunc("histo", createString(foodVector));
+
+		}
+		if (answer == 4) {
+			//exits loop if user enters 4
+			break;
+		}
+	}
+}
